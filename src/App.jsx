6fa0f0f877336a32035f2db95221f5ad7069e165
Rishabh Kanungo <rishabhkanungo731@gmail.com>
2025-09-17 16:18:28 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import TicTacToe from './Components/tictactoe/tictactoe'
+
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      
+      <TicTacToe/>
+      
+    </div>
+  )
+}
+
+export default App
